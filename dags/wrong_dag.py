e09c2ba148cbaf1ccbb4f178fea950f9c5491e30
Nikita Bakanchev <bakanchevn@gmail.com>
2023-05-17 20:31:10 +0300

    Fixed wrong_day syntax

--- dags/wrong_dag.py
+++ dags/wrong_dag.py
@@ -4,19 +4,19 @@ from airflow.operators.bash import BashOperator
 
 from datetime import datetime 
 
-def _check():
-    print("checking my data")
+def _run():
+    print("Running random Python task")
 
 dag = DAG('wrong_dag', schedule_interval='@daily', catchup=False)
 
 task_1 = PythonOperator(
         task_id='task_1',
-        python_callable=_check,
+        python_callable=_run,
         dag=dag
 )
 
 task_2 = BashOperator(
         task_id = 'task_2',
-        bash_command='echo "hello here"',
+        bash_command='echo "runing bash"',
         dag=dag
 )
